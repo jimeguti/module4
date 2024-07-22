0c0460fa54dc37aeb92855ac37429ae15034fec8
Monica Gutierrez Gutierrez <monicagutierrezgutierrez@MacBook-Air-de-Monica.local>
2024-07-21 20:39:25 -0600

    hi

---
+++ script.js
@@ -0,0 +1,16 @@
+// Wrap the entire contents of script.js inside of an IIFE
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // Loop over the names array and say either 'Hello' or "Good Bye"
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // Compare the 'firstLetter' to 'j' and call the appropriate method
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
